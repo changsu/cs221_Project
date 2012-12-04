25d4679ec7c9d2b6f5fdfbafce7f2bc8e47eec3b
Chang Su <changsu@stanford.edu>
2012-12-03 17:46:01 -0800

    before run real data

--- guessKL.m
+++ guessKL.m
@@ -1,17 +1,8 @@
-featureNum = 10;
-changeType = 4;
-sentenceNum = 100;
 totalKL = 0;
 
 L = rand(sentenceNum, changeType);
 % L_guess = rand(sentenceNum, changeType);
 L_guess = f_result;
-for i=1:sentenceNum
-    sumValue = sum(L(i,:));
-    L(i,:) = L(i,:)/sumValue;
-%     sum2 = sum(L_guess(i,:));
-%     L_guess(i,:) = L_guess(i,:)/sum2;
-end
 
 for n = 1:sentenceNum
     KL = 0;
